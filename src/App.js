4728712e429de3f92661fa637ad8527c9284b573
Pedro Sastre <pedro2000sastre@gmail.com>
2021-01-20 21:04:33 -0300

    2.About Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+
+import React from 'react';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>Film ar</h1>
+      <AboutUs/>
     </div>
   );
 }
